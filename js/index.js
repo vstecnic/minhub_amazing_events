dfcccc3cbf383f4948c577b2fccdb1390d7f302a
JuanGabrielSotoValenzuela <vstecnic@gmail.com>
2023-09-05 21:21:42 -0300

    filtro por checks corregido

--- js/index.js
+++ js/index.js
@@ -1,60 +1,77 @@
 const contenedor = document.getElementById('container_checks')
-const contenedorCategorias = document.getElementById('categorias')
 const buscador = document.getElementById('buscador')
 
-//category
 
-// let categorias= extraerCategorias()
+let categorias= extraerCategorias(data.events)
 // pintarSwitches(categorias, contenedorCategorias)
+pintarSwitches(categorias, contenedor)
+
+
+buscador.addEventListener("input", ()=>{
+  let filtro1 = filtrarPorTexto(data.events, buscador.value)
+  mostrarTarjetas(filtro1, contenedorTarjetas )
+})
+
+contenedor.addEventListener("change", ()=>{
+  console.log("me tocaste un check!")
+  let filtro1= filtrarPorCategoria(data.events)
+  console.log(filtro1)
+  mostrarTarjetas(filtro1,contenedorTarjetas)
+  // filtrarPorCategoria(data.events)
+  // let filtro2 = filtrarPorCategoria(data.events)
+    // mostrarTarjetas(filtro2, contenedorTarjetas)
+  })
+
+
+
+function crearSwitch(dato){
+     return `<div class="form-check form-switch col">
+                 <input class="form-check-input" type="checkbox" role="switch" id="${dato}" value="${dato}">
+                 <label class="form-check-label" for="${dato}">${dato}</label>
+            </div>`
+ }
+
+function pintarSwitches(arregloDeDatos, contenedor){ 
+      let html = ''
+      arregloDeDatos.forEach(elemento => {
+      html += crearSwitch(elemento)
+    })
+    contenedor.innerHTML = html
+}
+
+ function extraerCategorias(arreglo){
+     return arreglo.map(elemento => elemento.category).filter((categoria,indice, categorias) => categorias.indexOf(categoria) === indice)
+ }
+
+// filtros de categorías y de input search:
+      // Filtrado de texto----
+
+function filtrarPorTexto(arreglo, texto){
+  let arregloFiltrado = arreglo.filter(elemento => elemento.name.toLowerCase().includes(texto.trim().toLowerCase()) || elemento.description.toLowerCase().includes(texto.trim().toLowerCase()) || elemento.category.toLowerCase().includes(texto.trim().toLowerCase()))
+  return arregloFiltrado
+}
+
+function filtrarPorCategoria(arreglo){
+  let checkboxes= Array.from(document.getElementsByClassName("form-check-input"))
+  // let arrayCheckboxes = Array.from(checkboxes)
+  let checkAzules = checkboxes.filter( check => check.checked)
+  let valoresChecks = checkAzules.map(check => check.value)
+  if(valoresChecks.length==0){
+    return arreglo
+  }
+    let arregloFiltrado = arreglo.filter(objeto => valoresChecks.includes(objeto.category))
+    return arregloFiltrado
+}
 
 
 
 
-// function crearSwitch(dato,tipo){
-//     return `<div class="form-check form-switch col">
-//                 <input class="form-check-input ${tipo}" type="checkbox" role="switch" id="${dato}" value="${dato}">
-//                 <label class="form-check-label" for="${dato}">${dato}</label>
-//             </div>`
-// }
 
-// function pintarSwitches(arregloDeDatos, contenedor, tipo){
-//     let html = ''
-//     arregloDeDatos.forEach(elemento => {
-//         html += crearSwitch(elemento, tipo)
-//     })
-//     contenedor.innerHTML = html
-// }
-
-// function extraerCategorias(arreglo){
-//     return arreglo.map(elemento => elemento.category).filter((categoria,indice, categorias) => categorias.indexOf(categoria) === indice)
-// }
-
-
-
-
-
-
-
-
-// let categorias = extraerCategorias(frutas)
-
-
-// pintarSwitches(colores, contenedorColor, "colores")
-
-// pintarSwitches(temporadas, contenedorTemporada, "temporadas")
-
-// contenedorTemporada.addEventListener("change",filtroTriple)
-
-// contenedorColor.addEventListener("change",filtroTriple)
-
-// buscador.addEventListener("input",filtroTriple)
-
-
-
-
-
-
+//** pintar tarjetas  */
  let contenedorTarjetas = document.getElementById("contenido")
+
+
+
 
  mostrarTarjetas(data.events, contenedorTarjetas)
 
@@ -63,15 +80,17 @@ const buscador = document.getElementById('buscador')
      for (objeto of datosGenerales){
        tarjetas += crearCard(objeto)
      }
-     contenido.innerHTML = tarjetas
+     ubicacion.innerHTML = tarjetas
  }
 
 function crearCard(objeto){
    return    `<div class="card" style="width: 18rem;" >
     <img src="${objeto.image}" class="card-img-top p-2" alt="...">    <div class="card-body">
          <h5 class="card-title">${objeto.name}</h5>
-         <p class="card-text">${objeto.category}</b> <br> <b>Date: </b>${objeto.date}</p>
+         <p class="card-text">Category: ${objeto.category}</b> <br> <b>Date: </b>${objeto.date}
+         <br> <b>Category: </b>${objeto.category}</p>
          <a href="./pages/details.html?id=${objeto._id}" class="btn btn-primary">More Info...</a>
      </div>
  </div>` 
  }
+
