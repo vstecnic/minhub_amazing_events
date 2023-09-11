ae8dd6e97b9d206f24b8ab773814e016f10e3e1e
JuanGabrielSotoValenzuela <vstecnic@gmail.com>
2023-09-11 20:55:45 -0300

    details funcionando con fetch

--- js/detail.js
+++ js/detail.js
@@ -1,28 +1,44 @@
-//let parametros = new URLSearchParams(window.location.search)
 
+let url = 'https://mindhub-xj03.onrender.com/api/amazing'
+let eventos=[]
+let objeto = null
+let id = new URLSearchParams(window.location.search).get('id')
+console.log(id);
+const cardContainer = document.getElementById('cardContainer')
+const contenedor = document.getElementById('contenidoDetail')
 
-function buscarCardDetail(id){
-    let eventoEncontrado = data.events.find(elemento =>elemento._id==id)
-    console.log(eventoEncontrado)
-    return eventoEncontrado
- }
+traerDatos()
+
+function traerDatos() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            eventos= data.events
+            console.log(eventos)
+            cardDetail = eventos.find(elemento => elemento._id == id)
+            mostrarTarjetasUpcoming(cardDetail, contenedor)
+        })
+}
+
+function mostrarTarjetasUpcoming(objeto, ubicacion){
+  let tarjetas =""
+  tarjetas += crearCard(objeto) 
+  ubicacion.innerHTML = tarjetas
+}
+
 
 function crearCard(objeto){
-  return    `<div class="card" style="width: 18rem;" >
-   <img src="${objeto.image}" class="card-img-top p-2" alt="...">    <div class="card-body">
-        <h5 class="card-title">${objeto.name}</h5>
-        <p class="card-text"> <b>Category: </b>${objeto.category}</b> 
-        <p class="card-text"><b>Description: </b> ${objeto.description}</b> 
-        <p class="card-text"><b>Place: </b>${objeto.place}</b> 
-        <p class="card-text"><b>Capacity: </b>${objeto.capacity}</b> 
-        <p class="card-text"><b>Estimate: </b>${objeto.estimate}</b> 
-        <p class="card-text"><b>price: </b>${objeto.price}</b> 
-        
-    </div>
+ return    `<div class="card" style="width: 18rem;" >
+  <img src="${objeto.image}" class="card-img-top p-2" alt="...">    <div class="card-body">
+       <h5 class="card-title">${objeto.name}</h5>
+       <p class="card-text"><b>Date: </b>${objeto.date}
+       <br> <b>Category: </b>${objeto.category}</p>
+       <br> <b>Description: </b>${objeto.description}</p>
+       <br> <b>Place: </b>${objeto.place}</p>
+       <br> <b>Capacity: </b>${objeto.capacity}</p>
+       <br> <b>Estimate: </b>${objeto.estimate}</p>
+       <br> <b>Price: </b>${objeto.price}</p>
+   </div>
 </div>` 
 }
-function mostrarTarjeta(tarjeta, ubicacion){
-   
-    ubicacion.innerHTML = crearCard(tarjeta)
-}
-
