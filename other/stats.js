b7465858e21913230d8a398a52c7dce112a49249
JuanGabrielSotoValenzuela <vstecnic@gmail.com>
2023-09-12 13:40:35 -0300

    avance upcoming events statics, falta corregir datos con la logica de operaciones

--- other/stats.js
+++ other/stats.js
@@ -1,7 +1,6 @@
 let url = "https://mindhub-xj03.onrender.com/api/amazing";
 let eventos = [];
 let arrayCategory = [];
-let datosTabla2 = []
 
 eventStatics();
 upcomingStatics();
@@ -58,7 +57,6 @@ let largerCapacity = []; //new york --- mayor capacity ny2023
 let primerosLista = [];
 let mayorAtendance = []; //metallica
 let menorAtendance = []; //jurasic park
-let segundosLista = [];
 
 // Funciones:
 //*** REVENUES (Ganancia): Suma de la (asistencia o estimado) * precio de cada evento, de cada categoría. */
@@ -99,61 +97,32 @@ function pintarEstadisticas() {
 function extraerDatosSegundaTabla(datos) {
   //  if (objeto.date > fecha){
 
-  // datos.sort(
-  //   (a, b) =>
-  //     (b.assistance * 100) / b.capacity - (a.assistance * 100) / a.capacity
-  // );
-  // mayorAtendance.push(datos[0]);
-  // datos.sort(
-  //   (a, b) =>
-  //     (a.assistance * 100) / a.capacity - (b.assistance * 100) / b.capacity
-  // );
-  // menorAtendance.push(datos[0]);
-  // eventos.sort((a, b) => b.capacity - a.capacity);
-  // largerCapacity.push(datos[0]);
+  datos.sort(
+    (a, b) =>
+      (b.assistance * 100) / b.capacity - (a.assistance * 100) / a.capacity
+  );
+  mayorAtendance.push(datos[0]);
+  datos.sort(
+    (a, b) =>
+      (a.assistance * 100) / a.capacity - (b.assistance * 100) / b.capacity
+  );
+  menorAtendance.push(datos[0]);
+  eventos.sort((a, b) => b.capacity - a.capacity);
+  largerCapacity.push(datos[0]);
 
-  // datos.sort((a, b) => a.list - b.list);
-  // primerosLista.push(datos[0]);
-  let categories = [...new Set(datos.map(elemento => elemento.category))]
-  console.log(categories);
-  categories.forEach(categorie => {
-      let fila = {
-          Categories: categorie,
-          Revenues: 0,
-          Attendance: 0,
-      }
-
-      let sumaRevenues = 0
-      let datosPorCategories = datos.filter(elemento => elemento.category == categorie)
-      datosPorCategories.forEach(elemento => sumaRevenues+= +((elemento._id) * elemento.price) )
-      fila.Revenues = sumaRevenues
-      console.log(sumaRevenues)
-
-      let sumaAttendance = 0
-      datosPorCategories.forEach(elemento => sumaAttendance += +((elemento._id* 100) / elemento.capacity))
-      //datosPorPais.forEach(elemento => /* aca va el calculo dificil de lo que necesiten */  )
-      fila.Attendance = sumaAttendance
-      console.log(sumaAttendance)
-
-
-      segundosLista.push(fila)
-  })
-  console.log();
+  datos.sort((a, b) => a.list - b.list);
+  primerosLista.push(datos[0]);
 }
-
-
-
 
 function pintarEstadisticasSegundaTabla() {
   let html = "";
-  segundosLista.forEach(fila => html += `<tr>
-    
-                              <td>${fila.Categories}</td>
-                              <td>${fila.Revenues} </td>
-                              <td>${fila.Attendance}</td>                
-                         </tr>`
-                         )                      
-  
+  for (let i = 0; i < primerosLista.length; i++) {
+    html += `<tr>
+                            <td>${mayorAtendance[i].name}</td>
+                              <td>${menorAtendance[i].name} </td>
+                              <td>${largerCapacity[i].name}</td>                
+                         </tr>`;
+  }
   tablaUpcoming.innerHTML = html;
 }
 
